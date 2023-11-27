45f96ef436c4bb991fbe9bacbb9ec1a51ce9cf0b
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2023-11-27 14:54:21 -0500

    initial commit

---
+++ sorting/selectionSort.js
@@ -0,0 +1,12 @@
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) min = j;
+    }
+    if (i !== min) [arr[i], arr[min]] = [arr[min], arr[i]];
+  }
+  return arr;
+};
+
+console.log(selectionSort([5, 2, 4, -5, 1, 3]));
