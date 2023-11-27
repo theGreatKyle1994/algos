d4439b328b105bf25d935d292850c5c74c7f8315
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2023-11-27 15:32:44 -0500

    tweak to insertion sort

--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -1,7 +1,8 @@
 const insertionSort = (arr) => {
   for (let i = 1; i < arr.length; i++) {
     let currentVal = arr[i];
-    for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+    let j;
+    for (j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
       arr[j + 1] = arr[j];
     }
     arr[j + 1] = currentVal;
